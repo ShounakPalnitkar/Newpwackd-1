9b215d2c326ddc6ef15aed0a4f29c6656551624d
ShounakPalnitkar <ShounakPalnitkar1999@gmail.com>
2025-07-12 07:58:00 +0530

    Add files via upload

---
+++ sw (2).js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'ckd-tool-v1';
+const ASSETS_TO_CACHE = [
+  '/index-ckd.html',
+  '/',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://html2canvas.hertzen.com/dist/html2canvas.min.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js',
+  'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
